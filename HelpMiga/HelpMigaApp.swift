42a1e607b6ddfe9df658f7af1356468efdab1794
Ana Letícia <ana.branco@graduacao.ie.ufrj.br>
2023-01-24 14:16:40 -0300

    Initial Commit

---
+++ HelpMiga/HelpMigaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelpMigaApp.swift
+//  HelpMiga
+//
+//  Created by Ana Letícia Branco on 24/01/23.
+//
+
+import SwiftUI
+
+@main
+struct HelpMigaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
